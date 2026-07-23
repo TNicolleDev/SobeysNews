88e9eb627cfc4dc68a10954d8b72135d89522a83
Timothy N <Timothy.Nicolle.Dev@gmail.com>
2026-07-23 16:09:21 -0400

    first iteration

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
    return render_template("home.html")
+
+@app.route("/frozen")
+def frozen_in_time():
+   return render_template("frozen_in_time.html")
