57a7c9ec1475d81fd29b5e3fbc108d75749f5525
Tim Nicolle <tim.nicolle.dev@gmail.com>
2026-07-30 21:26:25 -0400

    hats added

--- blueprints/merch.py
+++ blueprints/merch.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template
 
 merch_bp = Blueprint('merch',__name__, url_prefix='/merch')
 
-@merch_bp.route("/merch")
-def merch():
-    return render_template("merch/merch.html")
+@merch_bp.route("/plushies")
+def plushies():
+    return render_template("merch/plushies.html")
+
+
+@merch_bp.route("/hats")
+def hats():
+    return render_template("merch/hats.html")
